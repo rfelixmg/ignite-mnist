89daafcd1288a0c313c269bec205f6794a696e4f
rfelixmg <rafael@aiml.team>
2020-11-16 11:43:08 +1030

    saving file

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 
 if __name__ == "__main__":
 
-    print("Hello world")
+
+	with open('/pvc/hello.text', 'w') as file_handler:
+		file_handler.write("Hello world from file!\n")
+
+	print("Hello world")
